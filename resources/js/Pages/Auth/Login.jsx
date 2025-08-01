94b509e8cc6e8182f319b3d03e24c3b3e21b4515
Lioncode_User_02 <eugmous@lioncode.gr>
2025-08-01 17:04:36 +0300

    Commit after adding the files to the folder

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,10 +1,10 @@
 export default function Login() {
     return (
         <div className="w-screen h-screen flex flex-col">
-            <header className=" h-24 text-4xl bg-teal-500 text-slate-900 relative">
+            <header className=" h-24 text-4xl font-bold bg-teal-500 text-teal-900 relative">
                 <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2">Header</div>
             </header>
-            <main className=" w-full flex-1 text-4xl font-bold bg-teal-50 text-indigo-600 relative">
+            <main className=" w-full flex-1 text-4xl font-bold bg-teal-50 text-teal-900 relative">
                 <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2">
                     This is the Login Page!
                 </div>
